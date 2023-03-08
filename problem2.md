77e567a606c9586eca3e7b7fdbd91436b14f5ff1
kowwaw <126995392+kowwaw@users.noreply.github.com>
2023-03-08 13:01:31 +0700

    Add files via upload

---
+++ problem2.md
@@ -0,0 +1,9 @@
+# Problem: 
+## Đề bài:
+Bằng phương pháp nhập và xuất, hãy nhập vào lần lượt điểm thi cuối kì đại số và giải tích của bạn.
+### Ví dụ:
+#### Input: 
+> 2 3
+#### Output:
+> Diem thi cuoi ki mon dai so cua ban la 2\
+> Diem thi cuoi ki mon giai tich cua ban la 3
---
+++ solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int daiSo, giaiTich;
+    cin  >> daiSo >> giaiTich;
+    cout << "Diem thi cuoi ki mon dai so cua ban la " << daiSo << endl;
+    cout << "Diem thi cuoi ki mon giai tich cua ban la " << giaiTich << endl;
+    return 0;
+}
